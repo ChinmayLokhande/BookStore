e717821341b71e3596b63a2dcd431284acbd5cdd
JayeshSabale <jayeshsabale22@gmail.com>
2024-02-11 12:47:08 +0530

    Testing Deployed site

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 function App() {
   return (
     <div>
-      <h1 style={{ textAlign: 'center' }}>The BookStore Project by Yousuf </h1>
+      <h1
+        style={{ textAlign: 'center', backgroundColor: 'lightgoldenrodyellow' }}
+      >
+        The BookStore Project by Jayesh
+      </h1>
     </div>
   )
 }
